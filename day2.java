bb1ed52add1f462a003c876ca3976b9aeb0dfd82
ImbarInformatika <imbarps16@gmail.com>
2023-10-11 09:29:06 +0800

    kedua

---
+++ day2.java
@@ -0,0 +1,12 @@
+package day1;
+public class day2 {
+    public static void main(String[] args) {
+        System.out.println("Mengalikan dua bilangan");
+        int jumlah, harga;
+        
+        harga = 5;
+        jumlah = 4;
+        
+        System.out.println("hasil : "+ harga * jumlah);
+    }
+}
