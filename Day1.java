30cd030e6695f8d353385a4519691221f48c96e7
ImbarInformatika <imbarps16@gmail.com>
2023-10-11 09:20:28 +0800

    test

---
+++ Day1.java
@@ -0,0 +1,10 @@
+package day1;
+public class Day1 {
+    public static void main(String[] args) {
+        int harga = 10;
+        int jumlah = 10;
+        
+        System.out.println(harga<jumlah);
+    }
+    
+}
